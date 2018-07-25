01e2799286eaba9464f6824375a569cc17d3bcb6
fernandojunior90 <fernandojunioreter@gmail.com>
2018-07-25 00:15:21 -0300

    funcionando sem socket

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-       Partida partida = new Partida();
+       Partida partida = new PartidaLocal();
+
+       partida.iniciar();
     }
 }
--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -1,40 +1,64 @@
 public class Tabuleiro {
 
-    public int[][] matriz;
+    public String[][] matriz;
 
 
     public Tabuleiro() {
 
         int count = 1;
-        matriz  = new int[3][3];
+        matriz  = new String[3][3];
         for(int linha=0 ; linha < 3 ; linha++){
             for(int coluna = 0; coluna < 3 ; coluna ++){
-                matriz[linha][coluna]=count;
+                matriz[linha][coluna]= Integer.toString(count);
                 count = count + 1;
             }
         }
     }
 
+    public String mostraTabuleiro(){
+        String mostraTabuleiro = "";
+
+        for (int i = 0; i< 3; i++){
+            for (int j = 0; j < 3; j++){
+                mostraTabuleiro += matriz[i][j];
+            }
+        }
+        imprimeTabuleiro();
+        return mostraTabuleiro;
+    }
+
+    public void atualizarTabuleiro(String situacao){
+        int cont = 0;
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j <3; j++){
+                matriz[i][j] = String.valueOf(situacao.charAt(cont));
+                cont++;
+            }
+        }
+        imprimeTabuleiro();
+
+    }
+
+
     public int verificaLinhas(){
 
         for (int linha = 0; linha<3; linha++){
-            if(matriz[linha][0] + matriz[linha][1] + matriz[linha][2] == -3){
+            if(Integer.parseInt(matriz[linha][0]) + Integer.parseInt(matriz[linha][1]) + Integer.parseInt(matriz[linha][2]) == -3){
                 return -1;
             }
-            if (matriz[linha][0] + matriz[linha][1] + matriz[linha][2] == 0){
+            if (Integer.parseInt(matriz[linha][0]) + Integer.parseInt(matriz[linha][1]) + Integer.parseInt(matriz[linha][2]) == 0){
                 return 0;
             }
         }
-
         return 10;
     }
 
     public int verificaColuna(){
         for (int coluna = 0; coluna<3; coluna++){
-            if (matriz[0][coluna] + matriz[1][coluna] + matriz[2][coluna] == -3){
+            if (Integer.parseInt(matriz[0][coluna]) + Integer.parseInt(matriz[1][coluna]) + Integer.parseInt(matriz[2][coluna]) == -3){
                 return -1;
             }
-            if (matriz[0][coluna] + matriz[1][coluna] + matriz[2][coluna] == 0 ){
+            if (Integer.parseInt(matriz[0][coluna]) + Integer.parseInt(matriz[1][coluna]) + Integer.parseInt(matriz[2][coluna]) == 0 ){
                 return 0;
             }
 
@@ -42,16 +66,16 @@ public class Tabuleiro {
       return 10;
     }
     public int verificaDiagonal(){
-        if(matriz[0][0] + matriz[1][1] + matriz[2][2] == -3){
+        if(Integer.parseInt(matriz[0][0]) + Integer.parseInt(matriz[1][1]) + Integer.parseInt(matriz[2][2]) == -3){
             return -1;
         }
-        if (matriz[2][0] + matriz[1][1] + matriz[0][2] == -3){
+        if (Integer.parseInt(matriz[2][0]) + Integer.parseInt(matriz[1][1]) + Integer.parseInt(matriz[0][2]) == -3){
             return -1;
         }
-        if(matriz[0][0] + matriz[1][1] + matriz[2][2] == 0){
+        if(Integer.parseInt(matriz[0][0]) + Integer.parseInt(matriz[1][1]) + Integer.parseInt(matriz[2][2]) == 0){
             return 0;
         }
-        if (matriz[2][0] + matriz[1][1] + matriz[0][2] == 0){
+        if (Integer.parseInt(matriz[2][0]) + Integer.parseInt(matriz[1][1]) + Integer.parseInt(matriz[0][2]) == 0){
             return 0;
         }
         return 10;
@@ -61,10 +85,10 @@ public class Tabuleiro {
 
         for (int linha = 0; linha<3; linha++){
             for (int coluna = 0; coluna<3; coluna++) {
-                if (matriz[linha][coluna] == -1){
+                if (Integer.parseInt(matriz[linha][coluna]) == -1){
                     System.out.print(" X ");
                 }
-                else if (matriz[linha][coluna] == 0){
+                else if (Integer.parseInt(matriz[linha][coluna]) == 0){
                     System.out.print(" O ");
                 }
                 else{
@@ -76,20 +100,5 @@ public class Tabuleiro {
             }
             System.out.println();
         }
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
